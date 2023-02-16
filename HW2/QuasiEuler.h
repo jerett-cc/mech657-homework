495f58c54b649d4518fb7c65fa9a9b4912ea3330
Jerett Cherry <jerett.cherry@colostate.edu>
2023-02-16 12:45:01 -0700

    added class description

--- HW2/QuasiEuler.h
+++ HW2/QuasiEuler.h
@@ -1,4 +1,10 @@
-
+/*
+ * This class includes all the functions to calculate local fluxes, RHS contributions and all functions needed to calculate
+ * physical quantities of interest from the solved quantities, relations include sound speed, temperature, and pressure.
+ *
+ * this class also senses shocks with a pressure sensor found in "Fundamentals Algorithms in Computational Fluid Dynamics"
+ * by Pulliam and Zingg 2014 pp. 97
+ */
 #include <cmath>
 
 #include "../Eigen/Dense"
